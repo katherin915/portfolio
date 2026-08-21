750447a582b52b566a2615fa1610f55a440d9b0b
Katherin Pandey <katherinpandey@gmail.com>
2026-08-22 00:19:19 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -149,8 +149,7 @@ base.
     }
 
 });
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
